1b24ef86ae7194756569ed84bb5c4d5cd3011f1c
Chris Grebeldinger <tinystranger@gmail.com>
2014-09-24 22:09:16 -0400

    Remove autocomplete code

--- app/src/main/java/com/tinystranger/lcbohelper/app/MainActivity.java
+++ app/src/main/java/com/tinystranger/lcbohelper/app/MainActivity.java
@@ -1,41 +1,28 @@
 package com.tinystranger.lcbohelper.app;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Location;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
-
-import com.google.zxing.integration.android.IntentResult;
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.GZIPInputStream;
 
 public class MainActivity extends FragmentActivity
         implements View.OnClickListener, LoaderManager.LoaderCallbacks<List<LCBOEntity>>
@@ -46,109 +33,6 @@ public class MainActivity extends FragmentActivity
     static Bitmap defaultThumbnail = null;
     static final double defaultLatitude = 43.64919634;
     static final double  defaultLongitude = -79.37793732;
-    class CompletionFetcher
-    {
-        private InputStream downloadUrl(String urlString) throws IOException {
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(10000 /* milliseconds */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("User-Agent", "LCBO/2.5 CFNetwork/672.1.14 Darwin/14.0.0");
-            conn.setRequestProperty("Accept", "*/*");
-            conn.setRequestProperty("Accept-Encoding", "gzip, deflate");
-            conn.setRequestProperty("Accept-Language", "en-us");
-            conn.setDoInput(true);
-            // Starts the query
-            conn.connect();
-            InputStream stream = conn.getInputStream();
-            return stream;
-        }
-
-        public ArrayList<String> retrieveResults(String constraint)
-        {
-            ArrayList<String> candidates = new ArrayList<String>();
-
-            String url = null;
-            try {
-                url = String.format(
-                        "http://www.lcbo.com/lcbo-ear/product/itemnameAutoComplete.do?language=EN&ITEM_NAME=%s&ITEM_NAME_RESULTS=15&ITEM_NAME_LENGTH=45"
-                        , URLEncoder.encode(constraint, "UTF-8"));
-
-                InputStream stream = null;
-                ProductCompletionParser parser = new ProductCompletionParser();
-                List<String> entries = null;
-                try {
-                    stream = new GZIPInputStream(downloadUrl(url));
-                    entries = parser.parse(stream);
-                    if (entries != null)
-                    {
-                        candidates.addAll(entries);
-                    }
-                    // Makes sure that the InputStream is closed after the app is
-                    // finished using it.
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (stream != null) {
-                        stream.close();
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return candidates;
-        }
-    }
-
-    class AutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
-        private List<String> mData;
-
-        public AutoCompleteAdapter(Context context, int textViewResourceId) {
-            super(context, textViewResourceId);
-            mData = new ArrayList<String>();
-        }
-
-        @Override
-        public int getCount() {
-            return mData.size();
-        }
-
-        @Override
-        public String getItem(int index) {
-            return mData.get(index);
-        }
-
-        @Override
-        public Filter getFilter() {
-            Filter myFilter = new Filter() {
-                @Override
-                protected FilterResults performFiltering(CharSequence constraint) {
-                    FilterResults filterResults = new FilterResults();
-                    if(constraint != null) {
-                        // A class that queries a web API, parses the data and returns an ArrayList<String>
-                        CompletionFetcher fetcher = new CompletionFetcher();
-                        mData = fetcher.retrieveResults(constraint.toString());
-                        // Now assign the values and count to the FilterResults object
-                        filterResults.values = mData;
-                        filterResults.count = mData.size();
-                    }
-                    return filterResults;
-                }
-
-                @Override
-                protected void publishResults(CharSequence contraint, FilterResults results) {
-                    if(results != null && results.count > 0) {
-                        notifyDataSetChanged();
-                    }
-                    else {
-                        notifyDataSetInvalidated();
-                    }
-                }
-            };
-            return myFilter;
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -156,9 +40,7 @@ public class MainActivity extends FragmentActivity
         setContentView(R.layout.fragment_main);
 
         final AutoCompleteTextView editText = (AutoCompleteTextView) findViewById(R.id.productSearchEditText);
-        // Doesn't work anymore
-        //editText.setAdapter(new AutoCompleteAdapter(this, android.R.layout.simple_dropdown_item_1line));
-        //
+
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
